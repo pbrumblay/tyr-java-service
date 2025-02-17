4dce0bc654c8b874129e6ff5c15fef071f16b344
Peter Brumblay <2146159+pbrumblay@users.noreply.github.com>
2025-02-17 13:47:20 -0700

    updating to trigger build 10

--- src/main/java/com/tyrconsulting/tyrjavarest/controller/HelloController.java
+++ src/main/java/com/tyrconsulting/tyrjavarest/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String sayHello() {
-        return "Hello, World! TESTING 1, 2, 3, 4, 5, 6, 7, 8, 9";  
+        return "Hello, World! TESTING 1, 2, 3, 4, 5, 6, 7, 8, 9, 10";  
     }
 
 }
